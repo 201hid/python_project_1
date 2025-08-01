a96d2edd30541e123ba11239910c71d0f43b49d3
tawhid <ah5229530@gmail.com>
2025-08-01 18:00:57 +1000

    update

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -4,3 +4,45 @@
 # TODO-4: Compare bids in dictionary
 
 
+
+bid_data={}
+
+
+
+def calculate_winner(users_dictionary):
+    name=max(users_dictionary, key=users_dictionary.get)
+    print(f'{name} has bid the highest amount {users_dictionary[name]}')
+
+
+def adduserdata():
+    more_user=1
+    while more_user==1:\
+
+        name=input('whats your name?')
+        bid=int(input("enter your bid amount?"))
+        bid_data[name]=bid
+        add_More=input("Do you want to add more user? Type Yes for adding and No for not adding").lower()
+        if add_More=="no":
+            more_user=0
+        elif add_More=="yes":
+            more_user=1
+            print("\n"*30)
+
+        else:
+            print("Enter Valid Input")
+    else:
+        print("thank you")
+
+    calculate_winner(bid_data)
+
+adduserdata()
+
+
+
+
+
+
+
+
+
+
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,28 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+
+def grades(number):
+    if number >= 91 and number <= 100:
+        return "Outstanding"
+    elif number >= 81 and number <= 90:
+        return "Exceeds Expectations"
+    elif number >= 71 and number <= 80:
+        return "Acceptable"
+    elif number <= 70:
+        return "fail"
+
+
+student_grades = {}
+
+for i in student_scores:
+    student_grades[i]=grades(student_scores[i])
+
+print(student_grades)
+
+
